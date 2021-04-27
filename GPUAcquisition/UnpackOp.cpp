df4c4462135fad61dd5dbe2c11faf81ad6d6b7fc
Paul Virally <pvirally@gmail.com>
2021-04-26 22:03:38 -0400

    Unpacking now supports both single and dual channel acquisition
    Added some functions to support single channel and dual channel data unpackign separately

--- GPUAcquisition/UnpackOp.cpp
+++ GPUAcquisition/UnpackOp.cpp
@@ -3,13 +3,19 @@
 #include "UnpackOp.hpp"
 #include "Utils.hpp"
 
-void gpu_unpack_data(const unsigned char* const raw_data, cufftReal* const chan_a, cufftReal* const chan_b, const size_t num_bytes, const cudaStream_t stream);
+void gpu_unpack_data_single_channel(const unsigned char* const raw_data, cufftReal* const chan, const size_t num_bytes, const cudaStream_t stream);
+void gpu_unpack_data_dual_channel(const unsigned char* const raw_data, cufftReal* const chan_a, cufftReal* const chan_b, const size_t num_bytes, const cudaStream_t stream);
 
 UnpackOp::UnpackOp(size_t num_bytes) : m_num_bytes(num_bytes) {}
 
 void UnpackOp::operate(unsigned char* input_buffer, cufftReal* chan_a, cufftReal* chan_b) {
 	// TODO: Different stream?
-	gpu_unpack_data(input_buffer, chan_a, chan_b, m_num_bytes, 0);
+	if (chan_b) {
+		gpu_unpack_data_dual_channel(input_buffer, chan_a, chan_b, m_num_bytes, 0);
+	}
+	else {
+		gpu_unpack_data_single_channel(input_buffer, chan_a, m_num_bytes, 0);
+	}
 }
 
 void UnpackOp::operate(void*, void*) {
